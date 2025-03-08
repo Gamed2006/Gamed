53742d096d40bd77852191451e1b5b9011436202
Gamed2006 <eloolpahyaa@gmail.com>
2025-03-08 08:42:18 +0200

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,9 +1,9 @@
 const settings = {
-  packname: 'Knight',
+  packname: 'Gamed',
   author: 'Bot',
-  botName: "Knight Bot",
-  botOwner: 'Professor', // Your name
-  ownerNumber: '917023951514', //Your number
+  botName: "Gamed Bot",
+  botOwner: 'Mohammed', // Your name
+  ownerNumber: '01220212628', //Your number
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
   commandMode: "public",
   description: "This is a bot for managing group commands and automating tasks.",
